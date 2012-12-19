6571f07ee934b2e9bdfa63c92b7ea6763e96de71
Master1916 <zppzzl@gmail.com>
2012-12-19 17:46:46 +0800

    first commit

---
+++ src/com/cyou/setting/SettingActivity.java
@@ -0,0 +1,17 @@
+package com.cyou.setting;
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SettingActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.setting);
+	}
+
+}
